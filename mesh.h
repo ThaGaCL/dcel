b09b71d91164817f858349beb65895069ed44d10
edsaibert <eduardasaibert@ufpr.br>
2025-04-27 19:06:30 -0300

    add face vertices vector

--- mesh.h
+++ mesh.h
@@ -7,26 +7,29 @@
 typedef struct {
     int x;
     int y;
-    HalfEdge* halfEdge;                 // Pointeiro para uma semi-aresta que tem v como origem (halfEdge(v))
+    HalfEdge* halfEdge;                     // Pointeiro para uma semi-aresta que tem v como origem (halfEdge(v))
 } Vertice;
 
 typedef struct {
-    Vertice* origin;                    // Ponteiro para o vértice de origem da semi-aresta (v)
-    HalfEdge* twin;                     // Ponteiro para a semi-aresta oposta
-    HalfEdge* next;                     // Ponteiro para a próxima semi-aresta na mesmo face
-    HalfEdge* prev;                     // Ponteiro para a semi-aresta anterior na mesma face
-    Face* leftFace;                     // Ponteiro para a face à esquerda da semi-aresta
+    Vertice* origin;                        // Ponteiro para o vértice de origem da semi-aresta (v)
+    HalfEdge* twin;                         // Ponteiro para a semi-aresta oposta
+    HalfEdge* next;                         // Ponteiro para a próxima semi-aresta na mesmo face
+    HalfEdge* prev;                         // Ponteiro para a semi-aresta anterior na mesma face
+    Face* leftFace;                         // Ponteiro para a face à esquerda da semi-aresta
 } HalfEdge;
 
 typedef struct {
-    HalfEdge* halfEdge;                 // Pointeiro para uma semi-aresta que tem f como face esquerda
+    HalfEdge* halfEdge;                     // Pointeiro para uma semi-aresta que tem f como face esquerda
 } Face;
 
 using namespace std;
 
-using VERTICES = vector<Vertice*>;      // Vetor de vértices
-using FACES = vector<Face*>;            // Vetor de faces
-using HALF_EDGES = vector<HalfEdge*>;   // Vetor de semi-arestas
+using VERTICES = vector<Vertice*>;          // Vetor de vértices
+using FACES = vector<Face*>;                // Vetor de faces
+using HALF_EDGES = vector<HalfEdge*>;       // Vetor de semi-arestas
+
+using FACE_VERTICES = vector<vector<int>>; // Vetor de vetores de inteiros, onde cada vetor interno representa os índices dos vértices de uma face
+
 
 class Mesh {
     public: 
@@ -46,6 +49,7 @@ class Mesh {
         VERTICES vertices;                
         FACES faces;
         HALF_EDGES halfEdges;
+        FACE_VERTICES faceVertices; 
 };
 
 #endif
