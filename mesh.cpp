e6bff7ccef2212171567870eae4b303cf58bafac
edsaibert <eduardasaibert@ufpr.br>
2025-04-28 20:03:02 -0300

    mesh methods

--- mesh.cpp
+++ mesh.cpp
@@ -27,17 +27,163 @@ void Mesh::load(){
         f->halfEdge = nullptr;
         faces.push_back(f);
 
+        faceVertices.clear();
         while (scanf("%d", &vertexIndex) == 1) {
             faceVertices.push_back(vertexIndex);
             if (getchar() == '\n') break; 
+        }
+
+        this->faceVertices.push_back(faceVertices);
+    }
+
+    constructEdges();
+
+}
+
+bool Mesh::faceDoesExist(int idx){
+    if (faces.size() == 0){
+        return false;
+    }
+
+    if (faces[idx]) return true;
+    return false;
+}
+
+Face* Mesh::createNewFace(HalfEdge* he){
+    Face* face = new Face;
+    face->halfEdge = he; 
+
+    faces.push_back(face);
+
+    return face;
+}
+
+void Mesh::printHalfEdge(HalfEdge* he){
+    std::cout << "HalfEdge: " << he << std::endl;
+    std::cout << "Origin: " << he->origin->x << " " << he->origin->y << std::endl;
+    std::cout << "Left Face: " << he->leftFace << std::endl;
+    std::cout << "Next: " << he->next << std::endl;
+    std::cout << "Prev: " << he->prev << std::endl;
+    std::cout << "Twin: " << he->twin << std::endl;
+}
+
+void printEdge(Edge* edge){
+    std::cout << "Origin: " << edge->origin->x << " " << edge->origin->y << std::endl;
+    std::cout << "Dest: " << edge->dest->x << " " << edge->dest->y << std::endl;
+    std::cout << "Face Index: " << edge->faceIdx << std::endl;
+    std::cout << std::endl;
+}
+
+void swap(Edge* &a, Edge* &b){
+    Edge* temp = a;
+    a = b;
+    b = temp;
+}
+
+/*
+    constrói arestas comuns para servir de auxiliar na criação de semi-arestas
+*/
+void Mesh::constructEdges(){
+    Edge* edge;
+    int idx;
+
+    for (unsigned int i = 0; i < nFaces; i++){
+        for (unsigned int j = 0; j < faceVertices[i].size(); j++){
+            edge = new Edge;
+
+            idx = faceVertices[i][j] - 1;           // os indices começam em 0 e as faces em 1
+            edge->origin = vertices[idx];
+
+            idx = faceVertices[i][(j+1) % faceVertices[i].size()] - 1;
+            edge->dest = vertices[idx];
+
+            edge->faceIdx = i;
+
+            edges.push_back(edge);
+        }
+    }
+
+    // 'ordena' as arestas para que, por exemplo, a aresta (v1, v2) fique antes da aresta (v2, v1)
+    for (unsigned int i = 0; i < edges.size(); i++){
+        for (unsigned int j = i+1; j < edges.size(); j++){
+            if (edges[i]->origin == edges[j]->dest && edges[i]->dest == edges[j]->origin){
+                swap(edges[i+1], edges[j]);
+            }
         }
     }
 
     constructHalfEdges();
 }
 
-void Mesh::constructHalfEdges(){}
+HalfEdge* Mesh::createHalfEdgeNode(int idx){
+    HalfEdge* he = new HalfEdge;
+    he->origin = edges[idx]->origin;
 
-void Mesh::constructVertices(){}
+    if (!faceDoesExist(edges[idx]->faceIdx)){
+        he->leftFace = createNewFace(he);
+    } else {
+        he->leftFace = faces[edges[idx]->faceIdx];
+    }
 
-void Mesh::constructFaces(){}
+    return he;
+}
+
+void Mesh::constructHalfEdges(){
+    HalfEdge* he;
+    HalfEdge* twin;
+    unsigned int size = edges.size();
+
+    for (unsigned int i = 0; i < size/2; i+=2){
+        he = createHalfEdgeNode(i);
+        twin = createHalfEdgeNode(i+1);
+
+        he->twin = twin;
+        twin->twin = he;
+
+        halfEdges.push_back(he);
+        halfEdges.push_back(twin);
+    }
+
+    // Faz a transversal da lista de semi-arestas para encontrar a próxima e a anterior
+    for (unsigned int i = 0; i < halfEdges.size(); i++){
+        findNext(halfEdges[i]);
+        findPrev(halfEdges[i]);
+        printHalfEdge(he);
+    }
+}
+
+/*
+    Para saber o próximo, é necessário checar a origem da simétrica da semi-aresta i
+    e então verificar na mesma face de i qual semi-aresta tem a mesma origem da simétrica
+*/
+void Mesh::findNext(HalfEdge* he){
+    HalfEdge* twin = he->twin;
+    HalfEdge* next = nullptr;
+
+    for (unsigned int i = 0; i < halfEdges.size(); i++){
+        if (halfEdges[i]->origin == twin->origin && halfEdges[i]->leftFace == he->leftFace){
+            next = halfEdges[i];
+            break;
+        }
+    }
+
+    he->next = next;
+}
+
+/*
+    Para saber o anterior, é necessário checar a origem da semi-aresta i
+    e então a simétrica que sai do mesmo vértice tem que ter a mesma face que a semi-aresta i
+*/
+void Mesh::findPrev(HalfEdge* he){
+    HalfEdge* twin = he->twin;
+    HalfEdge* prev = nullptr;
+
+    for (unsigned int i = 0; i < halfEdges.size(); i++){
+        if (halfEdges[i]->origin == he->origin && halfEdges[i]->leftFace == twin->leftFace){
+            prev = halfEdges[i];
+            break;
+        }
+    }
+
+    he->prev = prev;
+}
--- mesh.h
+++ mesh.h
@@ -13,6 +13,12 @@ typedef struct {
     HalfEdge* halfEdge;                     // Pointeiro para uma semi-aresta que tem v como origem (halfEdge(v))
 } Vertice;
 
+typedef struct {
+    Vertice* origin;                        // Ponteiro para o vértice de origem da aresta (v)
+    Vertice* dest;                          // Ponteiro para o vértice de destino da aresta (u)
+    int faceIdx;
+} Edge;
+
 typedef struct HalfEdge {
     Vertice* origin;                        // Ponteiro para o vértice de origem da semi-aresta (v)
     HalfEdge* twin;                         // Ponteiro para a semi-aresta oposta
@@ -30,6 +36,7 @@ using namespace std;
 using VERTICES = vector<Vertice*>;          // Vetor de vértices
 using FACES = vector<Face*>;                // Vetor de faces
 using HALF_EDGES = vector<HalfEdge*>;       // Vetor de semi-arestas
+using EDGES = vector<Edge*>;                // Vetor de arestas (auxiliar para a construção de semi-arestas)
 
 using FACE_VERTICES = vector<vector<int>>; // Vetor de vetores de inteiros, onde cada vetor interno representa os índices dos vértices de uma face
 
@@ -44,18 +51,25 @@ class Mesh {
         };
         void load();
         void constructHalfEdges();
-        void constructFaces();
-        void constructVertices();
+        void constructEdges();
 
     private:
         unsigned int nVertices;
         unsigned int nFaces;
         unsigned int nEdges;
 
+        bool faceDoesExist(int idx);
+        Face* createNewFace(HalfEdge* he);
+        HalfEdge* createHalfEdgeNode(int idx);
+        void findNext(HalfEdge* he);
+        void findPrev(HalfEdge* he);
+        void printHalfEdge(HalfEdge* he);
+
         VERTICES vertices;                
         FACES faces;
         HALF_EDGES halfEdges;
         FACE_VERTICES faceVertices; 
+        EDGES edges;                   
 };
 
 #endif
